2034ca41b7ae3d172138318d860e8b01bbeac4c1
hajinopro <hajinopro@gmail.com>
2022-10-29 20:43:02 +0900

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by 하진호 on 2022/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
